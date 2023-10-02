791ab1284a3d95de14d06ca8798af9469d049b1b
Dom Carlos Adriano <dogmalcarlos@gmail.com>
2023-10-02 20:37:52 -0300

    add sction sobre

--- script.js
+++ script.js
@@ -2,16 +2,6 @@ const menu = document.querySelector(".nav_menu");
 const menuBtn = document.querySelector(".open-menu-btn");
 const closeBtn = document.querySelector(".close-menu-btn");
 
-/*
-// change  navbar style on scroll
-window.addEventListener("scroll", () => {
-   document
-     .querySelector("nav")
-     .classList.toggle("window-scroll", window.scrollY > 0);
- });
-
-*/
-
 // show/hide nav menu
 
 menuBtn.addEventListener("click", () => {
